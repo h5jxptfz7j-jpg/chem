76611e7e3e7a1766f4a2a560072ff1e1170bd63a
Mer Win <h5jxptfz7j@privaterelay.appleid.com>
2026-04-30 02:38:17 +0700

    molecules.py

--- app/routers/molecules.py
+++ app/routers/molecules.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, HTTPException, Query, Depends 
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.database import get_session
